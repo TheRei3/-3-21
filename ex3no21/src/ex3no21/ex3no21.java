e06842a456c817bdd409604953c78d5eb1a47ad3
admin <admin@DESKTOP-8L2IG92>
2021-04-28 06:40:52 +0900

    ex3no21

---
+++ ex3no21/src/ex3no21/ex3no21.java
@@ -0,0 +1,12 @@
+package ex3no21;
+
+public class ex3no21 { // 110p
+	public static void main(String[] args) {
+		System.out.printf("10 == 10.05  \t %b%n",10     == 10.0f);
+		System.out.printf("'0' == 0     \t %b%n",'0'    == 0);
+		System.out.printf("'A' == 65    \t %b%n",'A'    == 65);
+		System.out.printf("'A' > 'B'    \t %b%n",'A'    > 'B');
+		System.out.printf("'A'+1 != 'B' \t %b%n",'A'+1  != 'B');
+	}
+
+}
